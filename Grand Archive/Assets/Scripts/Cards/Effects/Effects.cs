daf250a1109d8449f6f80606aa59859445232ac1
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-06-26 21:58:21 +1200

    started working on the effects but not sure if it is the correct effects

---
+++ Grand Archive/Assets/Scripts/Cards/Effects/Effects.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effects : ScriptableObject
+{
+    public virtual void Effect()
+    {
+        
+    }
+
+    public void Draw()
+    {
+        //deck.Draw();
+    }
+
+    public void Glimpse()
+    {
+        //deck.Glimpse();
+    }
+}
