d294ff466f0b7631c209f728ef90baeee397da38
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-07-08 23:08:02 +1200

    Work on deck builder

--- Grand Archive/Assets/Scripts/Cards/CardObject.cs
+++ Grand Archive/Assets/Scripts/Cards/CardObject.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardObject : MonoBehaviour
 {
+    private Color highlightColor = new Color32(130,130,130,255);
     public Card card;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<SpriteRenderer>().sprite = card.artwork;
+        GetComponent<Image>().sprite = card.artwork;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseOver()
     {
-        
+        Debug.Log("Over");
+        GetComponent<Image>().color = highlightColor;
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Exited");
+        GetComponent<Image>().color = Color.white;
     }
 }
