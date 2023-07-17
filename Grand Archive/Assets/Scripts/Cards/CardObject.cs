51cd3b172c11e0ef1669889fdd450e386266c006
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-07-18 09:12:33 +1200

    Adjusted deck builder scene

--- Grand Archive/Assets/Scripts/Cards/CardObject.cs
+++ Grand Archive/Assets/Scripts/Cards/CardObject.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CardObject : MonoBehaviour
+public class CardObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Color highlightColor = new Color32(130,130,130,255);
     public Card card;
@@ -13,15 +14,15 @@ public class CardObject : MonoBehaviour
         GetComponent<Image>().sprite = card.artwork;
     }
 
-    private void OnMouseOver()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("Over");
-        GetComponent<Image>().color = highlightColor;
+        GetComponent<Image>().color = Color.white;
+        gameObject.GetComponent<Transform>().localScale = new Vector3(1f, 1f, 0);
     }
 
-    private void OnMouseExit()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("Exited");
-        GetComponent<Image>().color = Color.white;
+        GetComponent<Image>().color = highlightColor;
+        gameObject.GetComponent<Transform>().localScale = new Vector3(1.1f, 1.1f, 0);
     }
 }
