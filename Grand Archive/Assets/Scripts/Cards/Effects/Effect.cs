174d2cd34525b8bfde0a754761e6e25b945d3bae
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-07-24 14:58:35 +1200

    Add hand to game scene

--- Grand Archive/Assets/Scripts/Cards/Effects/Effect.cs
+++ Grand Archive/Assets/Scripts/Cards/Effects/Effect.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum EffectTarget { self, champion, ally, unit }
+public enum EffectArea { all, opponentOnly, selfOnly}
 public abstract class Effect : ScriptableObject
 {
     public EffectTarget target = EffectTarget.unit;
+    public EffectArea area = EffectArea.all;
 
     public abstract void ApplyEffect(CardManager caster, CardManager target);
 }
